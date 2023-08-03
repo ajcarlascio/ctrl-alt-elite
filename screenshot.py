bb9f50f784ce6d869933a1f39fdd37f67bd57fa7
Anthony Carlascio <acarlascio@407etr.com>
2023-08-03 13:48:17 -0400

    Enhancing image capturing by removing bar and whitespace

--- screenshot.py
+++ screenshot.py
@@ -1,24 +1,37 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.action_chains import ActionChains
 from webdriver_manager.chrome import ChromeDriverManager
+from PIL import Image
 import datetime
 import time
  
 # using selenium headlessly
 options = Options()
 options.headless = True
- 
+
 # installing chrome driver
 driver = webdriver.Chrome(options=options)
+actions = ActionChains(driver)
 
 x = str(datetime.datetime.now())
  
 # getting the url and saving the screenshot
 driver.get('https://www.youtube.com/watch?v=VjvqGR3wsIs&ab_channel=VisitLaramie')
+nav = driver.find_element(By.ID,'container')
 driver.find_element(By.ID,'container').click()
-time.sleep(1)
+actions.move_to_element_with_offset(nav, 0, 0)
+time.sleep(3)
 driver.save_screenshot(x +' screenshot.png')
- 
+
+pngOpen = x +' screenshot.png'
+
+(left, upper, right, lower) = (120, 180, 1445, 919)
+pngOpen = Image.open(x +' screenshot.png')
+pngOpen = pngOpen.crop((left, upper, right, lower))
+pngOpen.save(x +' screenshot.png')
+
 # closing driver
-#driver.quit()
+time.sleep(1)
+driver.quit()
